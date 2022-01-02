c5d112bf73064095e341442e06418e6596f37b9a
programmer <internetov.ru@gmail.com>
2022-01-02 23:53:41 +0300

    first approach

--- src/ReverseNodesInKgroup/Solution.php
+++ src/ReverseNodesInKgroup/Solution.php
@@ -11,5 +11,38 @@ final class Solution
 {
     function reverseKGroup(ListNode $head, int $k): ListNode
     {
+        $c = $head;
+        $toReverse = [];
+        $reversed = $lastReversed = $tail = null;
+        while ($c->next) {
+            $toReverse[] = $c;
+            $c = $c->next;
+            if (count($toReverse) === $k) {
+                //reverse
+                for ($i = $k - 1; $i > 0; $i--) {
+                    $toReverse[$i]->next = $toReverse[$i - 1];
+                }
+
+                //save head of reversed
+                if ($reversed === null) {
+                    $reversed = $toReverse[$k - 1];
+                }
+
+                //connect previously reversed to new part
+                if ($tail) {
+                    $tail->next = $toReverse[$k - 1];
+                } else {
+                    $tail = $toReverse[0];
+                }
+
+                //connect current tail
+                $toReverse[0]->next = $c;
+
+                //then reset
+                $toReverse = [];
+            }
+        }
+
+        return $reversed;
     }
 }
--- src/ReverseNodesInKgroup/Test.php
+++ src/ReverseNodesInKgroup/Test.php
@@ -17,5 +17,11 @@ final class Test extends \PHPUnit\Framework\TestCase
         $k = 2;
         $outList = ListNode::fromArray([2, 1, 4, 3, 5]);
         $this->assertEquals($outList, $s->reverseKGroup($inList, $k));
+
+
+        $this->assertEquals(
+            ListNode::fromArray([3, 2, 1, 4, 5]),
+            $s->reverseKGroup(ListNode::fromArray([1, 2, 3, 4, 5]), 3)
+        );
     }
 }
