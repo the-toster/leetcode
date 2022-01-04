dcecbd755a6b1558c2cfc19eb25df30b9be90db7
programmer <internetov.ru@gmail.com>
2022-01-04 22:27:13 +0300

    naive recursion

--- src/Dynamic/HouseRobberII/Solution.php
+++ src/Dynamic/HouseRobberII/Solution.php
@@ -12,5 +12,21 @@ final class Solution
      */
     function rob(array $nums): int
     {
+        $dp = [
+            0 => 0,
+            1 => $nums[0] ?? 0,
+            2 => max($nums[0] ?? 0, $nums[1] ?? 0)
+        ];
+
+        if (isset($dp[count($nums)])) {
+            return $dp[count($nums)];
+        }
+
+        return max(
+            $nums[0] + $this->rob(array_slice($nums, 2, -1)),
+            $nums[1] + $this->rob(array_slice($nums, 3)),
+            $this->rob(array_slice($nums, 2))
+
+        );
     }
 }
