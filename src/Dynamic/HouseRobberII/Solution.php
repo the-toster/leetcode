70ba8ca46c8db1dd95d0c967c034e70b0d0f2ee5
programmer <internetov.ru@gmail.com>
2022-01-04 21:37:24 +0300

    WIP: DP daily 2

---
+++ src/Dynamic/HouseRobberII/Solution.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TheToster\Leetcode\Dynamic\HouseRobberII;
+
+
+final class Solution
+{
+    /**
+     * @param int[] $nums
+     */
+    function rob(array $nums): int
+    {
+    }
+}
---
+++ src/Dynamic/HouseRobberII/Test.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TheToster\Leetcode\Dynamic\HouseRobberII;
+
+
+use PHPUnit\Framework\TestCase;
+
+final class Test extends TestCase
+{
+    /** @test */
+    public function it_can_solve(): void
+    {
+        $s = new Solution();
+        $this->assertEquals(3, $s->rob([2, 3, 2]));
+        $this->assertEquals(4, $s->rob([1, 2, 3, 1]));
+        $this->assertEquals(3, $s->rob([1, 2, 3]));
+    }
+}
