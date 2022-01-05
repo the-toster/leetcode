8dc96a40372080db676a6e1bb0dbfc8713f5424c
programmer <internetov.ru@gmail.com>
2022-01-06 00:19:03 +0300

    naive recursion

--- src/PalindromePartitioning/Solution.php
+++ src/PalindromePartitioning/Solution.php
@@ -12,5 +12,24 @@ final class Solution
      */
     function partition(string $s): array
     {
+        if (strlen($s) === 1) {
+            return [[$s]];
+        }
+
+        $variants = [];
+        for ($i = 1; $i <= strlen($s); $i++) {
+            $sub = substr($s, 0, $i);
+            if ($this->isPalindrome($sub)) {
+                foreach ($this->partition(substr($s, $i)) as $subPartition) {
+                    $variants[] = array_merge([$sub], $subPartition);
+                }
+            }
+        }
+        return $variants;
+    }
+
+    private function isPalindrome(string $sub): bool
+    {
+        return $sub === strrev($sub);
     }
 }
