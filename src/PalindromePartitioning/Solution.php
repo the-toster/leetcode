5005f32e734f3e83f7e4b976f531c772adf54e66
programmer <internetov.ru@gmail.com>
2022-01-06 00:04:46 +0300

    Daily problem. With DP.

---
+++ src/PalindromePartitioning/Solution.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TheToster\Leetcode\PalindromePartitioning;
+
+
+final class Solution
+{
+    /**
+     * @return string[][]
+     */
+    function partition(string $s): array
+    {
+    }
+}
---
+++ src/PalindromePartitioning/Test.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TheToster\Leetcode\PalindromePartitioning;
+
+
+final class Test extends \PHPUnit\Framework\TestCase
+{
+    /** @test */
+    public function it_can_solve(): void
+    {
+        $s = new Solution();
+        $this->assertEquals([["a", "a", "b"], ["aa", "b"]], $s->partition('aab'));
+        $this->assertEquals([["a"]], $s->partition('a'));
+    }
+}
