a63722177c2c884b763e74bd74d8c60328167070
programmer <internetov.ru@gmail.com>
2022-01-07 22:30:15 +0300

    sollution from discussion tab, is it DP?

--- src/Dynamic/JumpGame/Solution.php
+++ src/Dynamic/JumpGame/Solution.php
@@ -11,31 +11,15 @@ final class Solution
      * @param int[] $nums
      * @return bool
      */
-    private $cache = [];
-
     function canJump(array $nums): bool
     {
-        $this->cache = [];
-        return $this->calc($nums, 0);
-    }
-
-    private function calc(array $nums, int $from): bool
-    {
-        if (isset($this->cache[$from])) {
-            return $this->cache[$from];
-        }
-
-        $maxJump = $nums[$from];
-        if ($maxJump >= count($nums) - $from - 1) {
-            return $this->cache[$from] = true;
-        }
-
-        for ($i = 1; $i <= $maxJump; $i++) {
-            if ($this->calc($nums, $from + $i)) {
-                return $this->cache[$from] = true;
+        $dist = 0;
+        for ($i = 0; $i <= $dist; $i++) {
+            $dist = max($dist, $i + $nums[$i]);
+            if ($dist >= count($nums) - 1) {
+                return true;
             }
         }
-
-        return $this->cache[$from] = false;
+        return false;
     }
 }
